b1e7ab440df7e5a510acc3905f7a7319e7a593e0
Sam Roshan Isaiah Manda <58909999+samroshan1310@users.noreply.github.com>
2024-02-29 20:18:01 -0500

    Add files via upload

---
+++ Automated Service Checker/ServiceChecker.ps1
@@ -0,0 +1,30 @@
+﻿$ServicesFilePath="D:\Sam Work\Courses\Powershell\services.csv"
+$LogPath="D:\Sam Work\Courses\Powershell\Logs"
+$LogFile="Services-$(Get-Date -Format "yyyy-MM-dd hh-mm").txt"
+$ServicesList=Import-Csv -Path $ServicesFilePath -Delimiter ','
+
+foreach($Service in $ServicesList){
+    $CurrentServiceStatus=(Get-Service -Name $Service.Name).status
+
+    if($Service.Status -ne $CurrentServiceStatus){
+        $Log="Service : $($Service.Name) is currently $CurrentServiceStatus, should be $($Service.Status)"
+        Write-Output $Log
+        Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
+
+        $Log="Setting $($Service.Name) to $($Service.Status)"
+        Write-Output $Log
+        Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
+        Stop-Service -Name $Service.Name -Force
+
+        $AfterServiceStatus=(Get-Service -Name $Service.Name).Status
+        if($Service.Status -eq $AfterServiceStatus){
+            $Log="Action was succesful Service $($Service.Name) is now $AfterServiceStatus"
+            Write-Output $Log
+            Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log    
+        }else{
+            $Log="Action failed Service $($Service.Name) is still $AfterServiceStatus, should be $($Service.Status)"
+            Write-Output $Log
+            Out-File -FilePath "$LogPath\$LogFile" -Append -InputObject $Log
+        }
+    }
+}
